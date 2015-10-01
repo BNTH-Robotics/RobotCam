7c66d61a9ba3baa80aafb1382b5b7755c74ee20c
Stobaugh Benjamin <legoben1998@gmail.com>
2015-10-01 04:12:06 -0500

    Add player and keyboard commands

--- data.js
+++ data.js
@@ -1,3 +1,6 @@
 var d = [
-    {"path":"videos/md1.mo"}
+    {"path":"videos/td1.webm", "title":"Test Drive - Part 1", "id":0},
+    {"path":"videos/td2.webm", "title":"Test Drive - Part 2", "id":1},
+    {"path":"videos/td3.webm", "title":"Test Drive - Part 3", "id":2},
+    {"path":"videos/td4.webm", "title":"Test Drive - Part 4", "id":3},
 ]
---
+++ js/main.js
@@ -0,0 +1,50 @@
+function showClip(data){
+    $("h2").text(data.title);
+    $("#v").attr("src", data.path);
+    $("#v").attr("vid", data.id);
+}
+
+function start(){
+    showClip(d[0])   
+}
+
+function updateTime(){
+    var v = document.getElementById('v')
+    //console.log(v)
+    
+    //Check if ended
+    if(v.ended){
+        var i = parseInt($("#v").attr("vid")) 
+        showClip(d[i+1])
+    }
+    
+    var p = v.currentTime / v.duration;
+    //console.log(p)
+    $("#progress-bar").css("width", (p*100)+"%")
+}
+
+start()
+setInterval(updateTime, 200)
+//updateTime()
+
+$(window).keyup(function(event){
+    if(event.keyCode == 191){
+        //Pop up help modal
+        
+    } else if (event.keyCode == 77){
+        //Mute or unmute   
+        document.getElementById('v').muted = !document.getElementById('v').muted;
+        
+    } else if (event.keyCode == 78){
+        var i = parseInt($("#v").attr("vid")) 
+        showClip(d[i+1])
+    } else if (event.keyCode == 80){
+        var i = parseInt($("#v").attr("vid")) 
+        showClip(d[i-1])
+        
+    } else if (event.keyCode == 83){
+        //Show choose modal
+        
+        
+    }
+});
